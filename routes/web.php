df0c7ce8a0d7f24a64cd9054837cf122c6f90c04
unknown <>
2025-03-06 00:39:19 -0500

    Settings in dashboard (design) focused in primary colors

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\VideoController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\CollectionController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
-
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -23,86 +24,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 });
-    Route::get('/videos', [VideoController::class, 'edit'])->name('video.edit');
-    Route::get('/manage-collection', [VideoController::class, 'index'])->name('video.edit');
-    Route::resource('posts', PostController::class)->only('index', 'store');
-    Route::resource('posts', PostController::class)->only('index', 'store');
 
+// Video routes
+Route::get('/videos', [VideoController::class, 'edit'])->name('video.edit');
+Route::get('/manage-collection', [VideoController::class, 'index'])->name('video.edit');
+Route::resource('videos', VideoController::class)->only('index', 'store');
+
+// Post routes
+Route::resource('posts', PostController::class)->only('index', 'store');
+
+// Image routes
+Route::resource('images', ImageController::class)->only('index', 'store');
+
+// Collection routes
+Route::get('collections/attach-image', [CollectionController::class, 'attachImage']);
+Route::resource('collections', CollectionController::class)->only('index', 'store');
 
 require __DIR__.'/auth.php';
-
-
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
-
-
-Route::resource('videos', App\Http\Controllers\VideoController::class)->only('index', 'store');
-
-Route::resource('images', App\Http\Controllers\ImageController::class)->only('index', 'store');
-
-Route::get('collections/attach-image', [App\Http\Controllers\CollectionController::class, 'attachImage']);
-Route::resource('collections', App\Http\Controllers\CollectionController::class)->only('index', 'store');
