b3329e513d0a7619c5e184dbde65d6a40cbff726
Alejandro <olartealejandro48mail.com>
2025-04-01 17:11:12 -0500

    refactor(assets): update and reorganize logo assets
    Replaced and reorganized various logo assets in the `resources/assets` directory. Updated the main `logo.svg` file with a simplified design and removed outdated logo files. Adjusted references to the new logo in multiple components to ensure consistency across the application.

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,7 +1,7 @@
 import { Head, Link } from '@inertiajs/react';
 
 import Bg from '@/../assets/background.svg';
-import Logo from '@/../assets/logo-v2.svg';
+import Logo from '@/../assets/logo.png';
 import { useEffect,useState } from 'react';
 
 export default function Welcome({ auth }) {
