7030ae5f1a3ddcec07342b8006300ee294c794b3
Alejandro <olartealejandro48mail.com>
2025-06-10 21:22:06 -0500

    style(Profile): improve button styling in update profile form
    Enhance the save button with gradient colors, hover effects, and transitions for better visual appeal

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -63,76 +63,88 @@ export default function UpdateProfileInformation({
     };
 
     return (
-        <section className={className}>
-            <header>
-                <h2 className="text-lg font-medium text-gray-900">Profile Information</h2>
-                <p className="mt-1 text-sm text-gray-600">
-                    Update your account's profile information and email address.
-                </p>
-            </header>
+      <section className={className}>
+        <header>
+          <h2 className="text-lg font-medium text-gray-900">
+            Profile Information
+          </h2>
+          <p className="mt-1 text-sm text-gray-600">
+            Update your account's profile information and email address.
+          </p>
+        </header>
 
-            <form onSubmit={handleSubmit(submit)} className="mt-6 space-y-6">
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
-                    <TextInput
-                        id="name"
-                        value={watchedName}
-                        onChange={(e) => register('name').onChange(e)}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                    />
-                    <InputError message={errors.name?.message} className="mt-2" />
+        <form onSubmit={handleSubmit(submit)} className="mt-6 space-y-6">
+          <div>
+            <InputLabel htmlFor="name" value="Name" />
+            <TextInput
+              id="name"
+              value={watchedName}
+              onChange={(e) => register("name").onChange(e)}
+              className="mt-1 block w-full"
+              autoComplete="name"
+            />
+            <InputError message={errors.name?.message} className="mt-2" />
+          </div>
+
+          <div>
+            <InputLabel htmlFor="email" value="Email" />
+            <TextInput
+              id="email"
+              value={watchedEmail}
+              onChange={(e) => register("email").onChange(e)}
+              type="email"
+              disabled={true}
+              className="mt-1 block w-full"
+              autoComplete="email"
+            />
+            <InputError message={errors.email?.message} className="mt-2" />
+          </div>
+
+          {mustVerifyEmail && user.email_verified_at === null && (
+            <div>
+              <p className="mt-2 text-sm text-gray-800">
+                Your email address is unverified.
+                <Link
+                  href={route("verification.send")}
+                  method="post"
+                  as="button"
+                  className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Click here to re-send the verification email.
+                </Link>
+              </p>
+
+              {status === "verification-link-sent" && (
+                <div className="mt-2 text-sm font-medium text-green-600">
+                  A new verification link has been sent to your email address.
                 </div>
-     
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-                       <TextInput
-                    id="email"
-                    value={watchedEmail}
-                    onChange={(e) => register('email').onChange(e)}
-                    type="email"
-                    disabled={true}
-                    className="mt-1 block w-full"
-                    autoComplete="email"
-                />
-                    <InputError message={errors.email?.message} className="mt-2" />
-                </div>
+              )}
+            </div>
+          )}
 
-                {mustVerifyEmail && user.email_verified_at === null && (
-                    <div>
-                        <p className="mt-2 text-sm text-gray-800">
-                            Your email address is unverified.
-                            <Link
-                                href={route('verification.send')}
-                                method="post"
-                                as="button"
-                                className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >
-                                Click here to re-send the verification email.
-                            </Link>
-                        </p>
-
-                        {status === 'verification-link-sent' && (
-                            <div className="mt-2 text-sm font-medium text-green-600">
-                                A new verification link has been sent to your email address.
-                            </div>
-                        )}
-                    </div>
-                )}
-
-                <div className="flex items-center gap-4">
-                    <PrimaryButton disabled={isSubmitting}>Save</PrimaryButton>
-                    <Transition
-                        show={!isSubmitting && !Object.keys(errors).length}
-                        enter="transition ease-in-out"
-                        enterFrom="opacity-0"
-                        leave="transition ease-in-out"
-                        leaveTo="opacity-0"
-                    >
-                        <p className="text-sm text-gray-600">Saved.</p>
-                    </Transition>
-                </div>
-            </form>
-        </section>
+          <div className="flex items-center gap-4">
+            <PrimaryButton
+              className="w-full 
+                transition-all 
+                duration-200 
+                flex items-center justify-center gap-3
+             bg-gradient-to-r from-orange-600 to-red-600 hover:from-orange-700
+             hover:to-red-700 transform hover:scale-[1.02] shadow-lg hover:shadow-xl"
+              disabled={isSubmitting}
+            >
+              Save
+            </PrimaryButton>
+            <Transition
+              show={!isSubmitting && !Object.keys(errors).length}
+              enter="transition ease-in-out"
+              enterFrom="opacity-0"
+              leave="transition ease-in-out"
+              leaveTo="opacity-0"
+            >
+              <p className="text-sm text-gray-600">Saved.</p>
+            </Transition>
+          </div>
+        </form>
+      </section>
     );
 }
