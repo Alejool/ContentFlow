c8ad63ca844bf0b212081b00cfb6ec42add88ac9
Alejandro <olartealejandro48mail.com>
2025-12-03 17:34:23 -0500

    feat: add theme and language switching components with a responsive container for welcome page links.

--- resources/js/Components/ThemeSwitcher.tsx
+++ resources/js/Components/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
-import { Sun, Moon } from "lucide-react";
 import { useTheme } from "@/Hooks/useTheme";
+import { Moon, Sun } from "lucide-react";
 import { useState } from "react";
 
 export default function ThemeSwitcher() {
@@ -42,8 +42,8 @@ export default function ThemeSwitcher() {
           <div
             className={`absolute -top-1 -right-1 w-5 h-5 flex items-center justify-center text-xs rounded-full 
               border border-gray-600 dar bg-gradient-to-br shadow-sm transition-all duration-300 ${
-              isAnimating ? "scale-0 rotate-90" : "scale-100"
-            }`}
+                isAnimating ? "scale-0 rotate-90" : "scale-100"
+              }`}
           >
             {isDark ? (
               <Sun className="w-3 h-3 " />
