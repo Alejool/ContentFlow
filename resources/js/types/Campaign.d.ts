dc24ce877b12725b2549add4d516b5c3b830cdea
Alejandro <olartealejandro48mail.com>
2025-12-03 13:43:07 -0500

    Add: Types to campaign and user

---
+++ resources/js/types/Campaign.d.ts
@@ -0,0 +1,12 @@
+
+export type Campaign = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  created_at: string;
+  updated_at: string;
+  goal?: string;
+  hashtags?: string;
+  slug?: string;
+};
