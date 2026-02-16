3553e65464be76afdea7716741bce2b47676f25d
Alejandro Olarte <olartealejandro48@gmail.com>
2026-02-16 16:09:28 -0500

    refactor(schedule): update ScheduleSection components

--- resources/js/Components/Content/modals/common/ScheduleSection.tsx
+++ resources/js/Components/Content/modals/common/ScheduleSection.tsx
@@ -35,9 +35,16 @@ const ScheduleSection: React.FC<ScheduleSectionProps> = ({
             type="checkbox"
             className="sr-only peer"
             checked={useGlobalSchedule}
-            onChange={(e) =>
-              !disabled && onGlobalScheduleToggle?.(e.target.checked)
-            }
+            onChange={(e) => {
+              if (!disabled) {
+                const isChecked = e.target.checked;
+                onGlobalScheduleToggle?.(isChecked);
+                // Clear the date when unchecking
+                if (!isChecked) {
+                  onScheduleChange("");
+                }
+              }
+            }}
             disabled={disabled}
           />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-300 dark:peer-focus:ring-primary-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600"></div>
