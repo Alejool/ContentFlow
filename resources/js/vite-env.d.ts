45a0ac89b309157188d0b48d0d91c456c74f303a
Alejandro Olarte <olartealejandro48@gmail.com>
2026-03-31 14:11:44 -0500

    feat(types): add Vite env type declarations for Reverb WebSocket variables

--- resources/js/vite-env.d.ts
+++ resources/js/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_REVERB_APP_KEY: string;
+  readonly VITE_REVERB_HOST: string;
+  readonly VITE_REVERB_PORT_WS: string;
+  readonly VITE_REVERB_PORT_WSS: string;
+  readonly VITE_REVERB_SCHEME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
